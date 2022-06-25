580ef113131e6be058f57f37e39a5cc13265b524
neXi0r <nexi0r.nex@gmail.com>
2022-06-25 18:42:22 +0200

    Create TeamsFormating.js

---
+++ TeamsFormating.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         MS Teams Formating
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  MS Teams SD formating button
+// @author       Alex 'neXi0r' Kielak
+// @match        https://qvcprod.service-now.com/incident.do?*
+// @grant        none
+// ==/UserScript==
+
+function TeamsText(event) {
+    let clip = 'P' + g_form.getValue('incident.priority') + ' - ' + g_form.getValue('sys_readonly.incident.number') + ' - ' + g_form.getValue('incident.short_description') + '\n\n' + 'Issue reported by: ' + 
+	navigator.clipboard.writeText(clip);
+}
+
+var priority_addons = document.querySelector("#element\\.incident\\.priority > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons")
+
+priority_addons.innerHTML += '<button id="myButton_Teams" style="white-space: nowrap" type="button" title="" data-original-title="Copy TEAMS" aria-expanded="false">Teams Text</button>';
+
+
+document.querySelector("#myButton_Teams").addEventListener ("click", TeamsText , false);
