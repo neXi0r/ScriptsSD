a2882c4590ebd924418d59533214fc419dc8a00f
neXi0r <nexi0r.nex@gmail.com>
2022-09-29 02:56:16 +0200

    Update TeamsFormating.js

--- TeamsFormating.js
+++ TeamsFormating.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         MS Teams Formating
+// @name         MS Teams Testing
 // @namespace    http://tampermonkey.net/
 // @version      0.1.9
 // @description  MS Teams SD formating button
