8f5f9bb6f34612709ef3654d22fd1ad7a06b09a9
neXi0r <nexi0r.nex@gmail.com>
2022-10-13 14:56:29 +0200

    Update SNowChatUtil.js

--- SNowChatUtil.js
+++ SNowChatUtil.js
@@ -25,23 +25,30 @@ var text8 = 'nine';
 var text9 = 'ten';
 
 var zNode = document.createElement ('div');
-zNode.innerHTML = '<button id="myButton0" type="button">1</button>' +
-    '<button id="myButton1" type="button">2</button>' +
-    '<button id="myButton2" type="button">3</button>' +
-    '<button id="myButton3" type="button">4</button>' +
-    '<button id="myButton4" type="button">5</button>' +
-    '<button id="myButton5" type="button">6</button>' +
-    '<button id="myButton6" type="button">7</button>' +
-    '<button id="myButton7" type="button">8</button>' +
-    '<button id="myButton8" type="button">9</button>' +
-    '<button id="myButton9" type="button">10</button>';
+zNode.innerHTML = '<button id="myButton0" type="button" title=' + text0 + '>1</button>' +
+    '<button id="myButton1" type="button" title=' + text1 + '>2</button>' +
+    '<button id="myButton2" type="button" title=' + text2 + '>3</button>' +
+    '<button id="myButton3" type="button" title=' + text3 + '>4</button>' +
+    '<button id="myButton4" type="button" title=' + text4 + '>5</button>' +
+    '<button id="myButton5" type="button" title=' + text5 + '>6</button>' +
+    '<button id="myButton6" type="button" title=' + text6 + '>7</button>' +
+    '<button id="myButton7" type="button" title=' + text7 + '>8</button>' +
+    '<button id="myButton8" type="button" title=' + text8 + '>9</button>' +
+    '<button id="myButton9" type="button" title=' + text9 + '>10</button>';
 zNode.setAttribute ('id', 'myContainer');
 document.body.appendChild (zNode);
 
 //--- Activate the newly added button.
-document.getElementById ("myButton").addEventListener (
-    "click", ButtonClickAction, false
-);
+document.getElementById ("myButton0").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton1").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton2").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton3").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton4").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton5").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton6").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton7").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton8").addEventListener ("click", ButtonClickAction, false);
+document.getElementById ("myButton9").addEventListener ("click", ButtonClickAction, false);
 
 function ButtonClickAction (zEvent) {
     /*--- For our dummy action, we'll just add a line of text to the top
@@ -56,7 +63,7 @@ function ButtonClickAction (zEvent) {
 GM_addStyle ( `
     #myContainer {
         position:               absolute;
-        top:                    0;
+        bottom:                    0;
         left:                   0;
         font-size:              20px;
         background:             orange;
