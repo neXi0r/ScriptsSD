b7bca25f60d4023a45312c7d321832ec156a395d
neXi0r <nexi0r.nex@gmail.com>
2022-10-18 17:48:13 +0200

    Update SNowChatUtil.js

--- SNowChatUtil.js
+++ SNowChatUtil.js
@@ -9,9 +9,6 @@
 // @downloadURL  https://raw.githubusercontent.com/neXi0r/ScriptsSD/main/SNowChatUtil.js
 // @updateURL    https://raw.githubusercontent.com/neXi0r/ScriptsSD/main/SNowChatUtil.js
 // ==/UserScript==
-/*--- Create a button in a container div.  It will be styled and
-    positioned with CSS.
-*/
 
 var text0 = 'one';
 var text1 = 'two';
@@ -24,46 +21,42 @@ var text7 = 'eight';
 var text8 = 'nine';
 var text9 = 'ten';
 
-var zNode = document.createElement ('div');
-zNode.innerHTML = '<button id="myButton0" type="button" title=' + text0 + '>1</button>' +
-    '<button id="myButton1" type="button" title=' + text1 + '>2</button>' +
-    '<button id="myButton2" type="button" title=' + text2 + '>3</button>' +
-    '<button id="myButton3" type="button" title=' + text3 + '>4</button>' +
-    '<button id="myButton4" type="button" title=' + text4 + '>5</button>' +
-    '<button id="myButton5" type="button" title=' + text5 + '>6</button>' +
-    '<button id="myButton6" type="button" title=' + text6 + '>7</button>' +
-    '<button id="myButton7" type="button" title=' + text7 + '>8</button>' +
-    '<button id="myButton8" type="button" title=' + text8 + '>9</button>' +
-    '<button id="myButton9" type="button" title=' + text9 + '>10</button>';
-zNode.setAttribute ('id', 'myContainer');
-document.body.appendChild (zNode);
-
-//--- Activate the newly added button.
-document.getElementById ("myButton0").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton1").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton2").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton3").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton4").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton5").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton6").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton7").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton8").addEventListener ("click", ButtonClickAction, false);
-document.getElementById ("myButton9").addEventListener ("click", ButtonClickAction, false);
-
-function ButtonClickAction (zEvent) {
-    /*--- For our dummy action, we'll just add a line of text to the top
-        of the screen.
-    */
-    var zNode = document.createElement ('p');
-    zNode.innerHTML = 'The button was clicked.';
-    document.getElementById ("myContainer").appendChild (zNode);
+function copyForChat(text){
+    let clip = text;
+	navigator.clipboard.writeText(clip);
 }
+
+var floatingdiv = document.createElement ('div');
+floatingdiv.innerHTML = '<button id="myButton0" type="button" title="' + text0 + '">1</button>' +
+    '<button id="myButton1" type="button" title="' + text1 + '">2</button>' +
+    '<button id="myButton2" type="button" title="' + text2 + '">3</button>' +
+    '<button id="myButton3" type="button" title="' + text3 + '">4</button>' +
+    '<button id="myButton4" type="button" title="' + text4 + '">5</button>' +
+    '<button id="myButton5" type="button" title="' + text5 + '">6</button>' +
+    '<button id="myButton6" type="button" title="' + text6 + '">7</button>' +
+    '<button id="myButton7" type="button" title="' + text7 + '">8</button>' +
+    '<button id="myButton8" type="button" title="' + text8 + '">9</button>' +
+    '<button id="myButton9" type="button" title="' + text9 + '">10</button>';
+floatingdiv.setAttribute ('id', 'myContainer');
+document.body.appendChild (floatingdiv);
+
+document.getElementById ("myButton0").addEventListener ("click", copyForChat(text0), false);
+document.getElementById ("myButton1").addEventListener ("click", copyForChat(text1), false);
+document.getElementById ("myButton2").addEventListener ("click", copyForChat(text2), false);
+document.getElementById ("myButton3").addEventListener ("click", copyForChat(text3), false);
+document.getElementById ("myButton4").addEventListener ("click", copyForChat(text4), false);
+document.getElementById ("myButton5").addEventListener ("click", copyForChat(text5), false);
+document.getElementById ("myButton6").addEventListener ("click", copyForChat(text6), false);
+document.getElementById ("myButton7").addEventListener ("click", copyForChat(text7), false);
+document.getElementById ("myButton8").addEventListener ("click", copyForChat(text8), false);
+document.getElementById ("myButton9").addEventListener ("click", copyForChat(text9), false);
+
 
 //--- Style our newly added elements using CSS.
 GM_addStyle ( `
     #myContainer {
         position:               absolute;
-        bottom:                    0;
+        bottom:                 0;
         left:                   0;
         font-size:              20px;
         background:             orange;
