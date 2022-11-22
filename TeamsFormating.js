dd034aad5e20e6c55a8d13e3ef4720f549890a0b
neXi0r <nexi0r.nex@gmail.com>
2022-11-22 12:34:11 +0100

    Update TeamsFormating.js

--- TeamsFormating.js
+++ TeamsFormating.js
@@ -70,15 +70,18 @@ function EBText(event) {
 		navigator.clipboard.writeText(clip);
 	}
 }
+
 function teamsOnHold(event) {
 	let clip = 'Hello. I\'m from IT Service Desk and I\'m contacting you regarding ' + g_form.getValue('sys_readonly.incident.number') + ' - ' + g_form.getValue('incident.short_description') + '.\nBefore we proceed, please provide additional information:\n';
 	navigator.clipboard.writeText(clip);
 }
+
 function PScmd_copy(event) {
     let clip = 'get-aduser -Identity ' + g_form.getReference('caller_id').user_name + ' -Server qrg.one -Properties homepostaladdress';
     navigator.clipboard.writeText(clip);
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function ME_button(event) {
     g_form.setValue('incident.assigned_to',g_user.userID, g_user.fullName);
     if (event.shiftKey) {
@@ -95,26 +98,24 @@ function ID_Button(event) {
 
 var priority_addons = document.querySelector("#element\\.incident\\.priority > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons");
 var state_addons = document.querySelector("#element\\.incident\\.state > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons");
-
-var webex_addons = document.querySelector("#element\\.incident\\.u_webex > col-xs-2 col-sm-3 col-lg-2 form-field-addons");
+var webex_addons = document.querySelector("#element\\.incident\\.u_webex > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons");
 var everbridge_notification_sent_addons = document.querySelector("#element\\.incident\\.u_everbridge_notification_sent > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons");
-
 var language_addons = document.querySelector("#element\\.incident\\.u_language > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons");
 var market_addons = document.querySelector("#element\\.incident\\.u_market > div.col-xs-2.col-sm-3.col-lg-2.form-field-addons");
-
 var on_behalf_of_addons = document.getElementById("viewr.incident.u_on_behalf_of").parentElement;
+
 var caller_addons = document.getElementById("viewr.incident.caller_id").parentElement;
 var assignedTo_addons = document.getElementById("viewr.incident.assigned_to").parentElement;
 
 
 priority_addons.innerHTML += '<button id="myButton_Teams1" style="white-space: nowrap" type="button" title="" data-original-title="Copy message for MS teams OPS-SD channel." aria-expanded="false">OPS chat MSG</button>';
-state_addons.innerHTML += '<button id="myButton_reachout" style="white-space: nowrap" type="button" title="" data-original-title="(Teams Reach out) Copy teams reach out message." aria-expanded="false">TR</button>';
+state_addons.innerHTML += '<button id="myButton_reachout" style="white-space: nowrap" type="button" title="" data-original-title="(Teams Reach out) Copy teams reach out message." aria-expanded="false">Reach out</button>';
 webex_addons.innerHTML += '<button id="myButton_Teams2" style="white-space: nowrap" type="button" title="" data-original-title="(Teams Text) Copy message for MS teams channel." aria-expanded="false">OPS chat MSG</button>';
 everbridge_notification_sent_addons.innerHTML += '<button id="myButton_Teams3" style="white-space: nowrap" type="button" title="" data-original-title="(EverBridge) Copy \'XX has been paged.\' message.\nShift click for just bridge address.\nCrtl click for Additional Individual" aria-expanded="false">Who was paged</button>';
-language_addons.innerHTML += '<button id="myButton_EB1" style="white-space: nowrap" type="button" title="" data-original-title="EB notification message" aria-expanded="false">NM</button>';
-market_addons.innerHTML += '<button id="myButton_EB2" style="white-space: nowrap" type="button" title="" data-original-title="EB notification message with bridge" aria-expanded="false">NMB</button>';
-
+language_addons.innerHTML += '<button id="myButton_EB1" style="white-space: nowrap" type="button" title="" data-original-title="EB notification message" aria-expanded="false">Notification Message</button>';
+market_addons.innerHTML += '<button id="myButton_EB2" style="white-space: nowrap" type="button" title="" data-original-title="EB notification message with bridge" aria-expanded="false">Notification Message + Bridge</button>';
 on_behalf_of_addons.innerHTML += '<button id="myButton_PSCMD" style="white-space: nowrap" type="button" title="" data-original-title="Copy QRG.ONE migration check PowerShell command" aria-expanded="false">QRG.ONE CHECK</button>';
+
 caller_addons.innerHTML += '<button id="myButton2" style="white-space: nowrap" type="button" title="" data-original-title="Copy User ID" aria-expanded="false">ID</button>';
 assignedTo_addons.innerHTML = assignedTo_addons.innerHTML+'<button class="form_action_button header action_context btn btn-default" id="personal1" style="white-space: nowrap" type="button" title="" value="sysverb_update_and_stay" id="sysverb_update_and_stay" data-action-name="sysverb_update_and_stay"  name="not_important" data-original-title="Assign to yourself" aria-expanded="false">ME</button>';
 
@@ -125,7 +126,7 @@ document.querySelector("#myButton_Teams3").addEventListener ("click", EBText , f
 document.querySelector("#myButton_EB1").addEventListener ("click", EBmsg , false);
 document.querySelector("#myButton_EB2").addEventListener ("click", EBmsg2 , false);
 document.querySelector("#myButton_reachout").addEventListener ("click", teamsOnHold , false);
-
 document.querySelector("#myButton_PSCMD").addEventListener ("click", PScmd_copy , false);
+
 document.querySelector("#myButton2").addEventListener ("click", ID_Button , false);
 document.querySelector("#personal1").addEventListener ("click", ME_button , false);
